4568b444ea6d3863d2ccdb02b6d31b08937f787f
sarthak.makhija <sarthak.makhija@gmail.com>
2021-12-24 00:12:56 +0530

    Sarthak | Solution to Path Sum2

---
+++ path_sum_2/PathSum.go
@@ -0,0 +1,57 @@
+package path_sum_2
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	var pathSumInner func(node *TreeNode, remainingSum int) [][]int
+
+	pathSumInner = func(node *TreeNode, remainingSum int) [][]int {
+		switch {
+		case node == nil:
+			return [][]int{}
+		case node.Left == nil && node.Right == nil && remainingSum-node.Val == 0:
+			return [][]int{{node.Val}}
+		}
+		merged := merge(pathSumInner(node.Left, remainingSum-node.Val), pathSumInner(node.Right, remainingSum-node.Val))
+		if len(merged) != 0 {
+			return addTo(merged, node.Val)
+		}
+		return merged
+	}
+	return pathSumInner(root, targetSum)
+}
+
+func merge(first [][]int, second [][]int) [][]int {
+	merged := copy2D(first)
+	for index := 0; index < len(second); index++ {
+		merged = append(merged, second[index])
+	}
+	return merged
+}
+
+func copy2D(elements [][]int) [][]int {
+	copied := make([][]int, len(elements))
+	for index := range elements {
+		copied[index] = make([]int, len(elements[index]))
+		copy(copied[index], elements[index])
+	}
+	return copied
+}
+
+func addTo(elements [][]int, element int) [][]int {
+	if len(elements) == 0 {
+		return append(elements, []int{element})
+	}
+	copied := copy2D(elements)
+	for index := 0; index < len(copied); index++ {
+		replacement := make([]int, len(copied[index])+1)
+		replacement[0] = element
+		copy(replacement[1:], copied[index])
+		copied[index] = replacement
+	}
+	return copied
+}
---
+++ path_sum_2/PathSum_test.go
@@ -0,0 +1,34 @@
+package path_sum_2
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPathSum1(t *testing.T) {
+	tree := &TreeNode{
+		Val:   5,
+		Left:  &TreeNode{Val: 4, Left: &TreeNode{Val: 11, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 2}}},
+		Right: &TreeNode{Val: 8, Left: &TreeNode{Val: 13}, Right: &TreeNode{Val: 4, Left: &TreeNode{Val: 5}, Right: &TreeNode{Val: 1}}},
+	}
+	result := pathSum(tree, 22)
+	expected := [][]int{{5, 4, 11, 2}, {5, 8, 4, 5}}
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf("Expected %v, received %v", expected, result)
+	}
+}
+
+func TestPathSum2(t *testing.T) {
+	tree := &TreeNode{
+		Val:   1,
+		Left:  &TreeNode{Val: 2},
+		Right: &TreeNode{Val: 3},
+	}
+	result := pathSum(tree, 5)
+	var expected [][]int
+
+	if len(result) != 0 {
+		t.Fatalf("Expected %v, received %v", expected, result)
+	}
+}
