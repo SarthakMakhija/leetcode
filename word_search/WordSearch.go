410f4e2ac72feae196ec6ec229cf369139442e8e
Sarthak Makhija <sarthakmakhija@Sarthaks-MacBook-Pro.local>
2022-04-15 00:01:21 +0530

    Sarthak | Changes the approach for word search slightly

--- word_search/WordSearch.go
+++ word_search/WordSearch.go
@@ -4,39 +4,37 @@ type position struct {
 	row, col int
 }
 
-func exist(board [][]byte, word string) bool {
-	var visitedPositions map[position]bool
-	rows, cols := len(board), len(board[0])
+func exist(board [][]byte, original string) bool {
+	rows := len(board)
+	cols := len(board[0])
 
-	var existInner func(currentPosition position, word string) bool
-	existInner = func(currentPosition position, word string) bool {
-		visitedPositions[currentPosition] = true
+	var existInner func(row, col int, remainingWord string, positions []position) bool
+	existInner = func(row, col int, remainingWord string, positions []position) bool {
+		positions = append(positions, position{row: row, col: col})
 		switch {
-		case len(word) == 0:
+		case len(remainingWord) == 0:
 			return true
-		case len(word) == 1 && board[currentPosition.row][currentPosition.col] == word[0]:
+		case row >= rows || col >= cols:
+			return false
+		case row < 0 || col < 0:
+			return false
+		case board[row][col] != remainingWord[0]:
+			return false
+		case len(remainingWord) == 1 && board[row][col] == remainingWord[0]:
 			return true
-		case currentPosition.row < 0 || currentPosition.col < 0:
-			return false
-		case currentPosition.row >= rows || currentPosition.col >= cols:
-			return false
-		case board[currentPosition.row][currentPosition.col] != word[0]:
-			return false
 		}
-		nextPositions := neighborsMatchingCharacterAtIndex(board, word, 1, currentPosition)
+		nextPositions := neighborsMatchingCharacterAtIndex(board, remainingWord, 1, position{row: row, col: col})
 		for _, nextPosition := range nextPositions {
-			if !visitedPositions[nextPosition] {
-				if existInner(nextPosition, word[1:]) {
+			if !isVisited(nextPosition, positions) {
+				if existInner(nextPosition.row, nextPosition.col, remainingWord[1:], positions) {
 					return true
 				}
 			}
 		}
-		delete(visitedPositions, currentPosition)
 		return false
 	}
-	for _, currentPosition := range firstCharacterIndicesIn(board, word) {
-		visitedPositions = make(map[position]bool)
-		if existInner(currentPosition, word) {
+	for _, currentPosition := range firstCharacterIndicesIn(board, original) {
+		if existInner(currentPosition.row, currentPosition.col, original, []position{}) {
 			return true
 		}
 	}
@@ -63,16 +61,15 @@ func neighborsMatchingCharacterAtIndex(board [][]byte, word string, index int, c
 		{row: currentPosition.row, col: currentPosition.col - 1},
 	}
 
+	var matchingPositions []position
 	if index < len(word) {
-		var matchingPositions []position
 		for _, neighborPosition := range neighborPositions {
 			if isPositionInBounds(board, neighborPosition) && board[neighborPosition.row][neighborPosition.col] == word[index] {
 				matchingPositions = append(matchingPositions, neighborPosition)
 			}
 		}
-		return matchingPositions
 	}
-	return []position{}
+	return matchingPositions
 }
 
 func isPositionInBounds(board [][]byte, currentPosition position) bool {
@@ -84,4 +81,13 @@ func isPositionInBounds(board [][]byte, currentPosition position) bool {
 		return false
 	}
 	return true
+}
+
+func isVisited(pos position, positions []position) bool {
+	for _, existingPosition := range positions {
+		if existingPosition.row == pos.row && existingPosition.col == pos.col {
+			return true
+		}
+	}
+	return false
 }
