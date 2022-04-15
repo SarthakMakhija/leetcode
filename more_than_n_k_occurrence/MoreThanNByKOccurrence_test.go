a8bef1bc2bddf430e0779fd604a808091a20f787
Sarthak Makhija <sarthakmakhija@Sarthaks-MacBook-Pro.local>
2022-04-15 13:27:07 +0530

    Sarthak | Uses sort in MoreThanNByKOccurrence_test.go

--- more_than_n_k_occurrence/MoreThanNByKOccurrence_test.go
+++ more_than_n_k_occurrence/MoreThanNByKOccurrence_test.go
@@ -2,13 +2,15 @@ package more_than_n_k_occurrence
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
 func TestMoreThanNByK1(t *testing.T) {
 	result := moreThanNByK([]int{30, 10, 20, 20, 20, 10, 40, 30, 30}, 4)
-	expected := []int{30, 20}
+	expected := []int{20, 30}
 
+	sort.Ints(result)
 	if !reflect.DeepEqual(expected, result) {
 		t.Fatalf("Expected %v, received %v", expected, result)
 	}
